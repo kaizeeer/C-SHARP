a87a3d87e2583d15bd8d1615ca9e65e4d650c1ac
kaizeeer <kaisserdiaz@gmail.com>
2024-09-12 10:13:48 +0800

    Add files via upload

---
+++ LabActivity1/try/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @try
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int myNum = Convert.ToInt32(textBox1.Text);
+            int firstN = 1;
+            int secondN = 1;
+            int stored = 0;
+            string output = "";
+            for (int ctr = 0; ctr < myNum; ctr++)
+            {
+                output += " " + stored;
+                if (ctr > 1)
+                {
+                    stored = firstN + secondN;
+                    secondN = firstN;
+                    firstN = stored;
+                }
+                else
+                {
+                    stored = firstN;
+                }    
+
+            }
+            label1.Text = output;
+        }
+    }
+}
